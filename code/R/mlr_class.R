7989cc5f055531c47d3584893a53c6341903293a
sacridini <eduardolacerdageo@gmail.com>
2020-09-17 02:20:37 -0300

    small fixes

--- code/R/mlr_class.R
+++ code/R/mlr_class.R
@@ -18,7 +18,7 @@ mlr::calculateConfusionMatrix(rfFoldCV$pred) # confusion matrix
 mlr::getFeatureImportance(rfModel) # Show the bands that contributed the most for the final result
 
 
-# Advanced - in this particular case its probably not ideal because we dont have enough input data
+# Advanced (optional) - in this particular case its probably not ideal because we dont have enough input data
 # Lets filter the most important predictors to speedup the model training
 fv <- mlr::generateFilterValuesData(rTask, method = "FSelectorRcpp_information.gain") # filter the most important bands
 mlr::plotFilterValues(fv)
@@ -26,10 +26,13 @@ filtered_task <- mlr::filterFeatures(rTask, method = "FSelectorRcpp_information.
 
 
 # Raster Classification ---------------------------------------------------
-r_df <- as.data.frame(as.matrix(r)) # transform the raster in data.frame
+in_raster <- stack("Documents/curso_rf/mosaic.tif")
+in_raster <- in_raster[[c(2,3,4,5,6,7)]]
+names(in_raster) <- c("B2", "B3", "B4", "B5", "B6", "B7")
+r_df <- as.data.frame(as.matrix(in_raster)) # transform the raster in data.frame
 r_df$value <- 0 # add and extra column with all zeros 
 pred_raster_rf <- predict(object = rfModel, newdata = r_df) # create the classification
-pred_raster <- r[[1]] # create the output raster
+pred_raster <- in_raster[[1]] # create the output raster
 pred_raster[] <- pred_raster_rf$data$response # set the values to the output raster
 plot(pred_raster) # plot the final classification raster
 writeRaster(pred_raster, "~/Documents/curso_rf/output_class.tif") # Save the output raster as .tif
