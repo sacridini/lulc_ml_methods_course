1b03d64d1b1b93ac1225e3d08777f9b470cab890
sacridini <eduardolacerdageo@gmail.com>
2020-09-29 23:07:07 -0300

    added ls8 rf simple class

---
+++ code/gee/lulc_gee_ls8_simple.js
@@ -0,0 +1,32 @@
+var geet = require('users/elacerda/geet:geet');
+var image_col = ee.ImageCollection(ls8.filterBounds(roi).filterDate('2019-01-01', '2019-12-31'))
+var mask_clouds = function(image) {
+  return (geet.cloudmask_ls8(image)); 
+}
+
+var masked_image_col = image_col.map(mask_clouds);
+var image = masked_image_col.median()
+
+var newfc = urbano.merge(agua).merge(floresta).merge(pasto)
+
+var input_features = image.sampleRegions({
+  collection: newfc,
+  properties: ['cobertura'],
+  scale: 30
+});
+
+// Train classifier(model) with training dataset
+var classifier = ee.Classifier.smileRandomForest(10).train({
+  features: input_features,
+  classProperty: 'cobertura',
+  inputProperties: ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
+});
+
+// Model/Classify with training dataset 
+var classified = image.classify(classifier);
+
+// Clip final result
+var classified_clip = classified.clip(roi);
+
+// Show classification result
+Map.addLayer(classified_clip)
