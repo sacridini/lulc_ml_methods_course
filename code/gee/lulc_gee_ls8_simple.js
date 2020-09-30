f000787c15ef7ed97e80bf8ac76a612e6a1354a0
sacridini <eduardolacerdageo@gmail.com>
2020-09-29 23:09:08 -0300

    small fixes

--- code/gee/lulc_gee_ls8_simple.js
+++ code/gee/lulc_gee_ls8_simple.js
@@ -1,14 +1,19 @@
 var geet = require('users/elacerda/geet:geet');
+
 var image_col = ee.ImageCollection(ls8.filterBounds(roi).filterDate('2019-01-01', '2019-12-31'))
+
 var mask_clouds = function(image) {
   return (geet.cloudmask_ls8(image)); 
 }
 
+// Create single landsat image
 var masked_image_col = image_col.map(mask_clouds);
 var image = masked_image_col.median()
 
+// Merge samples
 var newfc = urbano.merge(agua).merge(floresta).merge(pasto)
 
+// Collect Samples
 var input_features = image.sampleRegions({
   collection: newfc,
   properties: ['cobertura'],
